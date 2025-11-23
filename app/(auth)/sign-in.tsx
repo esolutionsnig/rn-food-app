8f68506141261b9e9160e8f9dbb45285df0a51a0
Ernest Ibeh <ibeh_ernest@yahoo.com>
2025-11-23 21:20:57 +0100

    Added auth screens

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,10 +1,58 @@
-import React from "react";
-import { Text, View } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
+import { Link, router } from "expo-router";
+import React, { useState } from "react";
+import { Alert, Text, View } from "react-native";
 
 const SignIn = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  const handleSubmit = async () => {
+    if (!form.email || !form.password)
+      return Alert.alert("Error", "Kindly fill the form with your details");
+
+    setIsSubmitting(true);
+
+    try {
+      Alert.alert("success", "User signed in successfully");
+      router.replace("/");
+    } catch (error: any) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
   return (
-    <View>
-      <Text>SignIn</Text>
+    <View className="gap-10 bg-white rounded-lg p-5 mt-5">
+      <CustomInput
+        placeholder="Enter your email"
+        value={form.email}
+        onChangeText={(text) => setForm((prev) => ({ ...prev, email: text }))}
+        label="Email"
+        keyboardType="email-address"
+      />
+      <CustomInput
+        placeholder="Enter your password"
+        value={form.password}
+        onChangeText={(text) =>
+          setForm((prev) => ({ ...prev, password: text }))
+        }
+        label="Password"
+        secureTextEntry={true}
+      />
+      <CustomButton
+        title="Sign In"
+        isLoading={isSubmitting}
+        onPress={handleSubmit}
+      />
+
+      <View className="flex flex-row justify-center mt-5 gap-2">
+        <Text className="base-regular text-gray-100">{`Don't have an account yet?`}</Text>
+        <Link href={"/sign-up"} className="base-bold text-primary">
+          Sign Up
+        </Link>
+      </View>
     </View>
   );
 };
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,5 +2,7 @@ export const appwriteConfig = {
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
   projectName: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_NAME,
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-  platform: "ng.cortiva.foodie",
+  platform: process.env.EXPO_PUBLIC_APPWRITE_PLATFORM,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  userCollection: process.env.EXPO_PUBLIC_APPWRITE_USER_COLLECTION,
 };
