991b244df00d559da1279375165a1ee59e940044
Ernest Ibeh <ibeh_ernest@yahoo.com>
2025-11-23 16:36:35 +0100

    Added AppWrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,6 @@
+export const appwriteConfig = {
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  projectName: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_NAME,
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: "ng.cortiva.foodie",
+};
